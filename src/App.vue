44692503aae491a9733dc99664dce097a989e02c
JustasPr <prasukas.j@gmail.com>
2025-08-11 22:35:15 +0300

    Input

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+        <h1>Weather App</h1>
+        <p>Best weather application in the world! haha</p>
+  </header>
+  <main>
+    <div class="place__input">
+      <input type="text" class="place__field" placeholder="Enter city name.." />
+    </div>
+  </main>
+</template>
